bbc33fe622dc64c19a03cf524e888a1120ddc292
Dan Wysocki <dwysocki@oswego.edu>
2015-04-17 01:17:30 -0400

    Updating game

--- rakushki/src/core/Main.js
+++ rakushki/src/core/Main.js
@@ -8,21 +8,103 @@ function boardFromArray(array, height, width) {
     return board;
 }
 
+function createMainMenu(singlePlayerFn, multiPlayerFn, settingsFn) {
+    var mainMenu = Cut
+        .column()
+        .spacing(10);
+    var buttonSinglePlayer = Buttons
+        .makeShellsButton("Single Player", "red")
+        .appendTo(mainMenu)
+        .on(Cut.Mouse.CLICK,
+            Util.partial(singlePlayerFn, mainMenu));
+    var buttonMultiPlayer = Buttons
+        .makeShellsButton("Multi Player", "yellow")
+        .appendTo(mainMenu)
+        .on(Cut.Mouse.CLICK,
+            Util.partial(multiPlayerFn, mainMenu));
+    var buttonSettings = Buttons
+        .makeShellsButton("Settings", "blue")
+        .appendTo(mainMenu)
+        .on(Cut.Mouse.CLICK,
+            Util.partial(settingsFn, mainMenu));
+    return mainMenu;
+}
+
+function createConfigMenu(root) {
+    var selectedGameType = SharikiGameType;
+
+    var configMenu = Cut
+        .column()
+        .spacing(10);
+    var gameTypeRow = Cut
+        .row()
+        .spacing(10)
+        .appendTo(configMenu);
+    var sharikiButton = Buttons.makeShellsButton("Shariki", "red")
+        .appendTo(gameTypeRow)
+        .on(Cut.Mouse.CLICK,
+            function() { selectedGameType = SharikiGameType; });
+    var bombiButton = Buttons.makeShellsButton("Bombi", "yellow")
+        .appendTo(gameTypeRow)
+        .on(Cut.Mouse.CLICK,
+            function () { selectedGameType = BombiGameType; });
+    var startButton = Buttons
+        .makeShellsButton("Start", "blue")
+        .appendTo(configMenu)
+        .on(Cut.Mouse.CLICK,
+            function() {
+                var width  = 8,
+                    height = 8;
+                var colors = ["red", "blue", "yellow", "green", "orange",
+                              "dark"];
+                var magnitudes = [null];
+                var specials = [null];
+                var types = [Shariki.NORMALSHELL];
+                var config = new Configuration(width, height,
+                                               colors, magnitudes,
+                                               specials, types);
+                var player1 = new Player();
+
+                var players = [player1];
+                var game = new selectedGameType(players, config);
+                var render = new Display(root, players, config);
+                game.setRender(render);
+                configMenu.hide();
+                render._createBoard(player1);
+            });
+
+    return configMenu;
+}
+
+
 var board1 = [
-    ["blue", "green", "blue"],
-    ["red", "blue", "red"],
+    ["blue", "green" , "blue"],
+    ["red" , "blue"  , "red" ],
     ["blue", "yellow", "blue"]
 ];
 
-var app = Cut(function(root,container) {
-    var config = new Configuration();
-    var player1 = new Player();
 
-//    var board = boardFromArray(board1, 3, 3);
-//    player1.setBoard(board);
-    var players = [player1];
-    var game = new SharikiGameType(players, config);
+var app = Cut(function(root,container) {
     Cut.Mouse(root, container);
-    var render = new Display(root, players, config);
-    game.setRender(render);
+
+    var configMenu = createConfigMenu(root)
+        .appendTo(root)
+        .hide()
+        .pin({alignX : 0.5,
+              alignY : 0.5});
+    
+
+    var singlePlayerFn = function(mainMenu) {
+        mainMenu.hide();
+        configMenu.show();
+    };
+    var multiPlayerFn = function(mainMenu) {};
+    var settingsFn = function(mainMenu) {};
+
+    var mainMenu = createMainMenu(singlePlayerFn,
+                                  multiPlayerFn,
+                                  settingsFn)
+        .appendTo(root)
+        .pin({alignX : 0.5,
+              alignY : 0.5});
 });
